d482ad9c8a523d7eeb32feb2140568c8e14bb65b
chippawah <charles.cantrell@me.com>
2018-11-09 14:32:18 -0700

    can get client through docker

--- server/src/server.js
+++ server/src/server.js
@@ -5,13 +5,6 @@ import helmet from 'helmet';
 import Query from './resolvers/Query';
 import Mutation from './resolvers/Mutation';
 
-// Setup the DB connection
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/sibi'
-mongoose.connect(uri, { useNewUrlParser: true });
-mongoose.connection
-  .once('open', ()=> console.log('MongoDB connection established!'))
-  .on('error', console.error.bind(console, 'MongoDB connection error!'))
-
 // Set up the GraphQL Server
 const server_opts = {
   port: process.env.GRAPHQL_PAYGROUND_PORT || 3000
@@ -21,8 +14,15 @@ const server = new GraphQLServer({
   resolvers: { Query, Mutation },
   context: (req) => Object.assign({}, req)
 });
-
-// Start the server
-server.start(server_opts, ({ port }) => {
-  console.log(`Server is listening on ${port}`)
-})
+// Setup the DB connection
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/sibi'
+mongoose.connect(uri, { useNewUrlParser: true });
+mongoose.connection
+  .once('open', ()=> {
+    console.log('MongoDB connection established!')
+    // Start the server
+    server.start(server_opts, ({ port }) => {
+      console.log(`Server is listening on ${port}`)
+    })
+  })
+  .on('error', console.error.bind(console, 'MongoDB connection error!'))
